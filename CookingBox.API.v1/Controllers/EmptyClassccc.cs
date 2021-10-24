cdda7aca46e7d17257328b05b2a8b91d3555073e
macbook <truongletann@gmail.com>
2021-10-25 03:08:44 +0700

    dd

---
+++ CookingBox.API.v1/Controllers/EmptyClassccc.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace CookingBox.API.v1.Controllers
+{
+    public class EmptyClassccc
+    {
+        public EmptyClassccc()
+        {
+            Console.WriteLine();
+        }
+    }
+}
